50ab8bf5723f87421d696197e22b5f94a50a539b
evanboardway <boardwae@mail.gvsu.edu>
2020-10-24 16:31:58 -0400

    Navigation button now shows a popup menu

---
+++ src/game/player-stats/nav_button.gd
@@ -0,0 +1,19 @@
+extends TextureButton
+
+
+var navigationMenu
+var active = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	navigationMenu = get_node("../NavMenu")
+
+
+func _on_NavButton_pressed():
+	if active:
+		navigationMenu.hide()
+		active = false
+	else:
+		navigationMenu.show()
+		active = true
+
